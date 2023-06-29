d32c5180ee86e465eb1df55bfc0a7e379ee47cd2
Rohan Jaiswal <62180133+jaiswalrohan8796@users.noreply.github.com>
2023-06-29 12:24:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
             })
             .catch((err) => {
                 console.log(err);
-                setLoading(true);
+                setLoading(false);
             });
     }, []);
 
